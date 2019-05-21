38ef33d1d4b72eba8e35aad5e1fd00d711e5c472
dartrhevan <vovasatunkin@mail.ru>
2019-05-21 10:58:08 +0500

    saving to word added

--- AudioPlayer/SongEditWindow.xaml.cs
+++ AudioPlayer/SongEditWindow.xaml.cs
@@ -8,10 +8,19 @@ using TagLib;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Shapes;/*
+using Microsoft.Office.Interop.Word.Application;
+using Microsoft.Office.Interop.Word.Document;
+using Microsoft.Office.Interop.Word.Selection;
+using Microsoft.Office.Interop.Word.Bookmark;
+using Microsoft.Office.Interop.Word.Range;*/
+using Microsoft.Office.Interop.Word;
+using Word = Microsoft.Office.Interop.Word;
+using Window = System.Windows.Window;
 
 namespace AudioPlayer
 {
@@ -50,19 +59,38 @@ namespace AudioPlayer
                 Year.Text = "0";
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void OkButtonClick(object sender, RoutedEventArgs e)
         {
-            Album.Songs[Index].Tag.Title = Title.Text;
-            Album.Songs[Index].Tag.Album = AlbumTitle.Text;
-            Album.Songs[Index].Tag.Year = uint.Parse(Year.Text);
-            Album.Songs[Index].Tag.Genres = new[] { Genre.Text.Replace(';', ' ') };
-            Album.Songs[Index].Tag.Performers = new[] { Performers.Text.Replace(';', ' ') };
-            Album.Songs[Index].Tag.Track = uint.Parse(AlbumIndex.Text);
+            File song = Album.Songs[Index];
+            song.Tag.Title = Title.Text;
+            song.Tag.Album = AlbumTitle.Text;
+            song.Tag.Year = uint.Parse(Year.Text);
+            song.Tag.Genres = new[] { Genre.Text.Replace(';', ' ') };
+            song.Tag.Performers = new[] { Performers.Text.Replace(';', ' ') };
+            song.Tag.Track = uint.Parse(AlbumIndex.Text);
+            //song.Dispose();
+            //song.Save();
             this.Close();
         }
-        private void Button_Click1(object sender, RoutedEventArgs e)
+        private void CancelButtonClick(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            File song = Album.Songs[Index];
+            var wdApp = new Word.Application();
+            //wdApp.Visible = true;
+            wdApp.Documents.Add();
+            var docum = wdApp.Documents.get_Item(1);
+            var openFolderDialog = new SaveFileDialog();
+            var res = openFolderDialog.ShowDialog();
+            if (!(res == System.Windows.Forms.DialogResult.Yes || res == System.Windows.Forms.DialogResult.OK && openFolderDialog.FileName != "")) return;
+
+            wdApp.Selection.Text = $"Название: {song.Tag.Title}\nАльбом: {song.Tag.Album}\nИсполнитель: {string.Join(", ", song.Tag.Performers)}\n ЖанрЖ {string.Join(", ", song.Tag.Genres)}\n Год: {song.Tag.Year}\n";
+            docum.SaveAs(openFolderDialog.FileName);
+            docum.Close();
         }
     }
 }
